9e772fefa5c6dda5b5055cbe373948002bc6cba6
Sultan Alsawaf <sultan@kerneltoast.com>
2023-07-03 17:22:38 -0700

    cpumask: Add optimized helpers when NR_CPUS fits in a long
    When NR_CPUS fits in a long, it's possible to use compiler built-ins to
    produce much faster code when operating on cpumasks compared to just using
    the generic bitops APIs.
    
    Therefore, add optimized helpers using compiler built-ins when NR_CPUS fits
    in a long. This also turns nr_cpu_ids into a compile-time constant for
    further optimization potential.
    
    Note that compared to the upstream cpumask rewrite with this feature, these
    optimized helpers perfectly preserve the semantics of the helpers they
    replace. And this change is much smaller than the upstream version.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/smp.c
+++ kernel/smp.c
@@ -512,6 +512,7 @@ static int __init nosmp(char *str)
 
 early_param("nosmp", nosmp);
 
+#if NR_CPUS > BITS_PER_LONG
 /* this is hard limit */
 static int __init nrcpus(char *str)
 {
@@ -525,6 +526,7 @@ static int __init nrcpus(char *str)
 }
 
 early_param("nr_cpus", nrcpus);
+#endif
 
 static int __init maxcpus(char *str)
 {
@@ -559,7 +561,9 @@ EXPORT_SYMBOL(nr_cpu_ids);
 /* An arch may set nr_cpu_ids earlier if needed, so this would be redundant */
 void __init setup_nr_cpu_ids(void)
 {
+#if NR_CPUS > BITS_PER_LONG
 	nr_cpu_ids = find_last_bit(cpumask_bits(cpu_possible_mask),NR_CPUS) + 1;
+#endif
 }
 
 void __weak smp_announce(void)
