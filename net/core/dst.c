8b3834027bca331046076e5f0cca4e569fb1eeeb
jsdizkcksv <3531236823@qq.com>
2025-01-18 10:04:46 +0800

    backport: Compatible with k4.9 kernel (2/2)
    * This patch serves as the final step in adapting the backport to the 4.9 kernel

--- net/core/dst.c
+++ net/core/dst.c
@@ -390,7 +390,8 @@ static void __metadata_dst_init(struct metadata_dst *md_dst, u8 optslen)
 	memset(dst + 1, 0, sizeof(*md_dst) + optslen - sizeof(*dst));
 }
 
-struct metadata_dst *metadata_dst_alloc(u8 optslen, gfp_t flags)
+struct metadata_dst *metadata_dst_alloc(u8 optslen, enum metadata_type type,
+					gfp_t flags)
 {
 	struct metadata_dst *md_dst;
 
@@ -398,7 +399,7 @@ struct metadata_dst *metadata_dst_alloc(u8 optslen, gfp_t flags)
 	if (!md_dst)
 		return NULL;
 
-	__metadata_dst_init(md_dst, optslen);
+	__metadata_dst_init(md_dst,optslen);
 
 	return md_dst;
 }
@@ -412,7 +413,8 @@ void metadata_dst_free(struct metadata_dst *md_dst)
 	kfree(md_dst);
 }
 
-struct metadata_dst __percpu *metadata_dst_alloc_percpu(u8 optslen, gfp_t flags)
+struct metadata_dst __percpu *
+metadata_dst_alloc_percpu(u8 optslen, enum metadata_type type, gfp_t flags)
 {
 	int cpu;
 	struct metadata_dst __percpu *md_dst;
