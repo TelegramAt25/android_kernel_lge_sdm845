d0a40daa9a8ebfc55becac4bfd78af4e78446972
fukiame <fukiame@proton.me>
2026-01-08 17:15:41 +0700

    net: sched: cls: guard unused vars behind CONFIG_MODULES
    Signed-off-by: fukiame <fukiame@proton.me>

--- net/sched/cls_api.c
+++ net/sched/cls_api.c
@@ -268,8 +268,10 @@ replay:
 		err = -ENOENT;
 		tp_ops = tcf_proto_lookup_ops(tca[TCA_KIND]);
 		if (tp_ops == NULL) {
+#ifdef CONFIG_MODULES
 			struct nlattr *kind = tca[TCA_KIND];
 			char name[IFNAMSIZ];
+#endif
 
 			if (cl)
 				cops->put(q, cl);
