4247ac9d58db66da9d59d560686a66d9f27dd4fb
fukiame <fukiame@proton.me>
2026-01-08 16:43:31 +0700

    scsi: sd: drop unused sd_check_events
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -1410,7 +1410,7 @@ static int media_not_present(struct scsi_disk *sdkp,
 	return 0;
 }
 
-#ifdef CONFIG_MACH_LGE
+#if 0
 /**
  *	sd_check_events - check media events
  *	@disk: kernel device descriptor
