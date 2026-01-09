4cd5cbd14315bb4ac54f3c034090c6beb2dd0aa5
fukiame <fukiame@proton.me>
2026-01-09 11:57:41 +0700

    msm: ipa: gsb: fry debug
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/platform/msm/ipa/ipa_clients/ipa_gsb.c
+++ drivers/platform/msm/ipa/ipa_clients/ipa_gsb.c
@@ -37,20 +37,10 @@
 
 #define IPA_GSB_DBG(fmt, args...) \
 	do { \
-		pr_debug(IPA_GSB_DRV_NAME " %s:%d " fmt, \
-			__func__, __LINE__, ## args); \
-		IPA_IPC_LOGGING(ipa_get_ipc_logbuf(), \
-			IPA_GSB_DRV_NAME " %s:%d " fmt, ## args); \
-		IPA_IPC_LOGGING(ipa_get_ipc_logbuf_low(), \
-			IPA_GSB_DRV_NAME " %s:%d " fmt, ## args); \
 	} while (0)
 
 #define IPA_GSB_DBG_LOW(fmt, args...) \
 	do { \
-		pr_debug(IPA_GSB_DRV_NAME " %s:%d " fmt, \
-			__func__, __LINE__, ## args); \
-		IPA_IPC_LOGGING(ipa_get_ipc_logbuf_low(), \
-			IPA_GSB_DRV_NAME " %s:%d " fmt, ## args); \
 	} while (0)
 
 #define IPA_GSB_ERR(fmt, args...) \
