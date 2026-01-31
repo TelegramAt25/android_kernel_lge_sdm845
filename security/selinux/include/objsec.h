9873156dcb40b71c0f36ad78595fe82ffdf792a6
fukiame <fukiame@proton.me>
2026-02-01 00:09:38 +0700

    [SQUASH] selinux: revert backports
    its leaking everywhere?
    
    Revert "UPSTREAM: selinux: sidtab reverse lookup hash table, commit 25387ca23232580f58603c7f17981cae31766b3b.
    Revert "UPSTREAM: selinux: avoid atomic_t usage in sidtab, commit 1e6663433f6dfde233d9207345c4c016b3a9325b.
    Revert "UPSTREAM: selinux: check sidtab limit before adding a new entry, commit d80431b1311ba7baf5bcf8a2bca537fe268f5fbb.
    Revert "UPSTREAM: selinux: fix context string corruption in convert_context(), commit e9a76ffb33332faa84e272e1f918e54ee8dc1fbb.
    Revert "BACKPORT: selinux: overhaul sidtab to fix bug and improve performance, commit 1764e12f6527d613f297893853b7ea88b17e5e28.
    Revert "UPSTREAM: selinux: refactor mls_context_to_sid() and make it stricter, commit e277d750f8a56c1cda238cec94c3c484cb0b8e20.
    Revert "UPSTREAM: selinux: Cleanup printk logging in services, commit b9ff4f8f62706d29c84b101b95adcd7df90b7ce6.
    Revert "UPSTREAM: scsi: ilog2: create truly constant version for sparse, commit 3929dca5b1d277eaf6e03cdb84a58adb6efad49e.
    Revert "BACKPORT: selinux: use separate table for initial SID lookup, commit f8b4d70f81e1923bf354b1496ae3d40bb6c5bee6.
    Revert "UPSTREAM: selinux: make "selinux_policycap_names[]" const char *, commit ff034abdd73436377792a198af330224c0731a57.
    Revert "UPSTREAM: selinux: refactor sidtab conversion, commit c233ced1f9f18f45c3b4094da916381fdf4cfbc3.
    Revert "BACKPORT: selinux: wrap AVC state, commit d7a44d9f870ec8c659c27ce54e162825cc19d6b7.
    Revert "BACKPORT: selinux: wrap selinuxfs state, commit d903591bbbc3b83b8282e4a135cd8480699c488f.
    Revert "UPSTREAM: selinux: rename the {is,set}_enforcing() functions, commit 6020e20870f722e90d17cd39c98859c505162067.
    Revert "BACKPORT: selinux: wrap global selinux state, commit f8b7f4973e7ec1d9128ba5151ceccede73fe9749.
    Revert "UPSTREAM: selinux: Use kmem_cache for hashtab_node, commit 69f5bc76b964361dbf9259ac56ce7c532c84dfc0.
    Revert "UPSTREAM: selinux: Generalize support for NNP/nosuid SELinux domain transitions, commit 7c5fa0cc00a1f412d4485f350deaa29e3989dd7a.
    Revert "BACKPORT: selinux: log policy capability state when a policy is loaded, commit cced3b0161230e809f09df51bd7a593238c91046.
    Revert "UPSTREAM: selinux: Adjust four checks for null pointers, commit 30f8fc57b56db8b9bc073a381b0112eb6ab6b8a8.
    Revert "UPSTREAM: selinux: Use kmalloc_array() in hashtab_create(), commit b53a8d041dfc31babbbae1a107a044ea29f705d5.
    Revert "UPSTREAM: selinux: use kmem_cache for ebitmap, commit ffc7a10fdf127367589f65426d75bc5a9bdbd052.
    Revert "UPSTREAM: selinux: clean up cred usage and simplify, commit 64eb5dfdc764998572a87d80a11bf95690862ca6.
    Revert "fixup! selinux: wrap AVC state, commit 98bd1b7cb3e4adbac5cc39b1d4caa3f77c29d4a0.
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- security/selinux/include/objsec.h
+++ security/selinux/include/objsec.h
@@ -38,16 +38,6 @@ struct task_security_struct {
 	u32 sockcreate_sid;	/* fscreate SID */
 };
 
-/*
- * get the subjective security ID of the current task
- */
-static inline u32 current_sid(void)
-{
-	const struct task_security_struct *tsec = current_security();
-
-	return tsec->sid;
-}
-
 enum label_initialized {
 	LABEL_INVALID,		/* invalid or not initialized */
 	LABEL_INITIALIZED,	/* initialized */
@@ -145,5 +135,7 @@ struct key_security_struct {
 struct bpf_security_struct {
 	u32 sid;  /*SID of bpf obj creater*/
 };
+
+extern unsigned int selinux_checkreqprot;
 
 #endif /* _SELINUX_OBJSEC_H_ */
